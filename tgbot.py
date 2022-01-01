875ac0dde735e349e6dbf4b2c6b86da84547cbcd
enaix <enaix@protonmail.com>
2022-01-01 20:45:40 +0300

    Fix multiple minor bugs

--- tgbot.py
+++ tgbot.py
@@ -16,7 +16,9 @@ class CBot():
         self.users_count = 0
         self.init_timer()
         self.web_addr = None
-        self.user_greet = 'Hello, {user.mention_markdown_v2()}\! Your account has been logged on the server\. Please login and whitelist your user ID\.'
+        self.allow = set(self.bot_set["admins"]) & set(self.bot_set["allowed_ssh_ids"]) &\
+                set(self.bot_set["allowed_web_ids"]) & set(self.bot_set["allowed_webtoken_ids"])
+        self.user_greet = 'Hello, {user}\! Your account has been logged on the server\. Please login and whitelist your user ID\.'
     
         self.updater = Updater(self.bot_env["token"])
         self.dispatcher = self.updater.dispatcher
@@ -25,10 +27,6 @@ class CBot():
         self.dispatcher.add_handler(CommandHandler("web", self.address))
         self.dispatcher.add_handler(CommandHandler("webtoken", self.get_web_token))
         
-    def main_loop(self):
-        self.updater.start_polling()
-        self.updater.idle()
-
     def load_env(self, filename):
         with open(filename, 'r') as f:
             self.bot_env = json.load(f)
@@ -58,7 +56,7 @@ class CBot():
             if self.users_count < self.bot_set["spam_threshold"]:
                 return False
             else:
-                for u in self.bot_env["admins"]:
+                for u in self.bot_set["admins"]:
                     context.bot.send_message(chat_id=u, text="Warning! The spam attack has been detected. The new users logging will be reenabled in "\
                             + str(self.bot_set["spam_cooldown"]) + " seconds.")
                 self.bot_set["enable_logging"] = False
@@ -66,13 +64,13 @@ class CBot():
                 t.start()
                 return True
 
-    def check_and_log(self, user, notify=True):
+    def check_and_log(self, user, ctx, notify=True):
         self.users_count += 1
-        if not self.check_spam(context):
+        if not self.check_spam(ctx):
             with open(os.path.join(self.working_dir, "users_log"), 'a') as f:
                 f.write(time.strftime("%a %d %b %H:%M:%S", time.localtime()) + ' ' + str(user) + '\n')
-            if notify and self.bot_env["notify_on_login"]:
-                for u in self.bot_env["admins"]:
+            if notify and self.bot_set["notify_on_login"]:
+                for u in self.bot_set["admins"]:
                     context.bot.send_message(chat_id=u, text="New login attempt: user "\
                             + str(user["id"]) + " (" + str(user["first_name"]) + " " + str(user["last_name"]) + ")")
             return True
@@ -80,13 +78,13 @@ class CBot():
 
     def start(self, update: Update, context: CallbackContext) -> None:
         user = update.effective_user
-        if user.id in self.bot_env["allowed_ids"]:
+        if user.id in self.allow:
             update.message.reply_markdown_v2(
                 fr'Welcome back, {user.mention_markdown_v2()}\!',
             )
         else:
-            if self.check_and_log(update.effective_user):
-                update.message.reply_markdown_v2(self.user_greet.format(user=user))
+            if self.check_and_log(update.effective_user, context):
+                update.message.reply_markdown_v2(self.user_greet.format(user=user.mention_markdown_v2()))
 
     def run_cmd(self, cmd, *args):
         try:
@@ -99,44 +97,45 @@ class CBot():
         if not self.bot_set["enable_ssh"]:
             return
         user = update.effective_user
-        if user.id in self.bot_env["allowed_ssh_ids"]:
+        if user.id in self.bot_set["allowed_ssh_ids"]:
             ssh_out = self.run_cmd(self.bot_set["ssh_script_path"])
             update.message.reply_markdown_v2(str("`") + str(ssh_out.decode()).strip() + str("`"))
         else:
             print("Access denied:", user)
-            self.check_and_log(update.effective_user, notify=False)
+            self.check_and_log(update.effective_user, context, notify=False)
 
     def address(self, update: Update, _: CallbackContext) -> None:
         if not self.bot_set["enable_server"]:
             return
         user = update.effective_user
-        if user.id in self.bot_env["allowed_web_ids"]:
+        if user.id in self.bot_set["allowed_web_ids"]:
             web_out = self.run_cmd(self.bot_set["web_script_path"])
             self.web_addr = str(web_out.decode()).strip()
             update.message.reply_markdown_v2("Webserver address: " + "https://" + self.web_addr)
         else:
             print("Access denied:", user)
-            self.check_and_log(update.effective_user, notify=False)
+            self.check_and_log(update.effective_user, ctx, notify=False)
 
     def get_web_token(self, update: Update, _: CallbackContext) -> None:
         if not self.bot_set["enable_webtoken"]:
             return
         user = update.effective_user
-        if user.id in self.bot_env["allowed_webtoken_ids"]:
+        if user.id in self.bot_set["allowed_webtoken_ids"]:
             token = self.run_cmd(self.bot_set["webtoken_script_path"])
-            if self.bot_env["glue_webtoken"] and self.web_addr is not None:
+            if self.bot_set["glue_webtoken"] and self.web_addr is not None:
                 update.message.reply_markdown_v2("Webserver address: " + "https://" + self.web_addr\
                         + "/?token=" + str(token.decode()).strip())
             else:
                 update.message.reply_markdown_v2("Webserver token: " + "https://" + str(token.decode()).strip())
         else:
             print("Access denied:", user)
-            self.check_and_log(update.effective_user, notify=False)
+            self.check_and_log(update.effective_user, context, notify=False)
 
 
 def main():
     bot = CBot()
-    bot.main_loop()
+    bot.updater.start_polling()
+    bot.updater.idle()
 
 if __name__ == '__main__':
     main()
