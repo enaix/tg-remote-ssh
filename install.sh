626d3135bc5b68a6e8b5833ec3fde2171ed593a2
enaix <enaix@protonmail.com>
2022-01-01 19:42:15 +0300

    Fix install.sh sed parsing

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 if (( $UID == 0 )); then
 	echo "Please run as non-priviledged user"
@@ -15,13 +16,13 @@ echo "Parsing .service files..."
 for f in $FILES; do
 	sed -i "s/cur_user/$CUR_USER/g" $f
 	sed -i "s/cur_group/$CUR_USER/g" $f
-	sed -i "s/working_dir/$WORKING_DIR/g" $f
+	sed -i 's|working_dir|'"$WORKING_DIR"'|g' $f
 done
 
-echo "Copying services to /etc/systemd/system/multi-user.target.wants ..."
+echo "Copying services to /usr/lib/systemd/system ..."
 
 for f in $FILES; do
-	sudo cp $f /etc/systemd/system/multi-user.target.wants/$f
+	sudo cp $f /usr/lib/systemd/system/
 done
 
 echo "Done! Now you may enable/disable these services. View README.md for details"
